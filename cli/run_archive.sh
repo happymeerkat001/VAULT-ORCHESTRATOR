0de78be103021e798144c3d6b784fb8f8349b472
Ang Li <mr.angli@gmail.com>
2026-05-13 09:13:33 -0500

    • feat: auto-ingest daily note YouTube URLs

--- cli/run_archive.sh
+++ cli/run_archive.sh
@@ -2,4 +2,5 @@
 cd /Users/leon/Documents/Code/Obsidian-vault-orchestrator/cli
 sleep 5
 /usr/local/bin/python3 archive_youtube.py
+/usr/local/bin/python3 daily_note_youtube.py
 /usr/local/bin/python3 scrape_notes.py
